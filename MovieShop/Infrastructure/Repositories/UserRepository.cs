4f0f545778b7bee6c58a4505fbea9a760b4bd8d3
Vishal Pange <vishalpange4513@gmail.com>
2024-07-22 19:37:37 -0600

    MovieShop MVC Project

---
+++ MovieShop/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Models;
+using ApplicationCore.Contracts.Repository;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(MovieShopDbContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        
+    }
+
+}
---
+++ MovieShop/MovieShop/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieShop.Models;
+using Infrastructure.Services;
+using ApplicationCore.Contracts.Services;
+
+namespace MovieShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+
+        private readonly IMovieService _movieService;
+
+        public HomeController(IMovieService movieService)
+        {
+            _movieService = movieService;
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _movieService.GetTopRevenueMovies();
+            return View(movies);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
